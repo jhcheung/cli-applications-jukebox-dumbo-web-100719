44207568696233344421cd2edf928b3424f9c915
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-09-29 18:04:03 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,10 +24,7 @@ end
 def play(playlist)
   puts "Please enter a song name or number:"
   user_response = gets.strip
-  playlist.length.times do |n|
-    if user_response.to_i <= playlist.length
-  end 
-  if user_response.to_i <= playlist.length
+  if user_response.to_i <= playlist.length || playlist.any?(user_response)
     puts playlist[user_response.to_i - 1]
   else
     puts "Invalid input, please try again"
